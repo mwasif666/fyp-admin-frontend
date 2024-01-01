28b7d34e003a819bc69ab2df6992df9ce22699e4
WaleedGhori <waleedghori4@gmail.com>
2024-01-01 18:06:57 +0500

    prod

--- src/Pages/AllProductsDetails.js
+++ src/Pages/AllProductsDetails.js
@@ -64,29 +64,7 @@ const AllProductDetails = () => {
     localStorage.setItem("Products", JSON.stringify(productListClone));
   };
 
-  const openEditModal = (index) => {
-    setEditIndex(index);
-    setEditName(productsList[index].productName);
-    setEditPrice(productsList[index].productPrice);
-    setEditImage(productsList[index].productImage);
-    setEditModal(true); // Open the edit modal
-  };
-
-  const saveEditedProduct = () => {
-    if (editIndex !== null) {
-      let productListClone = [...productsList];
-      productListClone[editIndex] = {
-        ...productListClone[editIndex],
-        productName: editName,
-        productPrice: editPrice,
-        productImage: editImage,
-      };
-      setProductsList(productListClone);
-      localStorage.setItem("Products", JSON.stringify(productListClone));
-      setEditIndex(null);
-      setEditModal(false); // Close the edit modal
-    }
-  };
+  const saveEditedProduct = () => {};
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -181,31 +159,134 @@ const AllProductDetails = () => {
     setViewModal(true);
   };
 
+  // this is get product code
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [prodTitle, setProdTitle] = useState(" ");
+  const [prodDesc, setProdDesc] = useState(" ");
+  const [prodPrice, setProdPrice] = useState(" ");
+  const [prodImg1, setProdImg1] = useState(" ");
+  const [prodImg2, setProdImg2] = useState(" ");
+  const [prodQty, setProdQty] = useState(" ");
+  const [prodSize, setProdSize] = useState(" ");
+  const [prodColor, setProdColor] = useState(" ");
+  const [prodCategory, setProdCategory] = useState(" ");
+  const [productFeatured, setProductFeatured] = useState(" ");
+  const [id, setId] = useState("");
 
-const [product , setProduct] = useState([])
-const [loading  , setLoading]= useState(true)
-  const getProduct = async()=>{
-    setLoading(true)
-    let response  = await fetch(`http://localhost:5000/api/prod/v1/getproduct`,{
-      method:"GET",
+  const getProduct = async () => {
+    setLoading(true);
+    let response = await fetch(`http://localhost:5000/api/prod/v1/getproduct`, {
+      method: "GET",
       headers: {
         "auth-token":
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6IjY1ODMzZWRiZjU4MzE3ZGMzYzg5MTQyMCIsImlhdCI6MTcwMzEwMDE1OH0.xqQ7ykJAGEwW_-DOuTps0GCpK4nU4Hra40d-g4TGGR8",
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6IjY1ODMzZWRiZjU4MzE3ZGMzYzg5MTQyMCIsImlhdCI6MTcwMzEwMDE1OH0.xqQ7ykJAGEwW_-DOuTps0GCpK4nU4Hra40d-g4TGGR8", // this token comes from you cookies or localstorage
       },
-    })
-    let res  = await response.json();
-    console.log(res);
-    if(res.success){
-      setLoading(false)
-      setProduct(res.product)
-      console.log(product);
-    }else{
-      alert(res.status)
+    });
+    let res = await response.json();
+    if (res.success) {
+      setLoading(false);
+      setProduct(res.product);
+    } else {
+      alert(res.status);
     }
+  };
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  // Edit product api code comes here
+
+  const openEditModal = (index, id) => {
+    setEditModal(true);
+    setId(id); // Open the edit modal
+  };
+
+  const handChange = (e) => {
+    if (e.target.name === "prodTitle") setProdTitle(e.target.value);
+    if (e.target.name === "prodDesc") setProdDesc(e.target.value);
+    if (e.target.name === "prodPrice") setProdPrice(e.target.value);
+    if (e.target.name === "prodQty") setProdQty(e.target.value);
+    if (e.target.name === "prodSize") setProdSize(e.target.value);
+    if (e.target.name === "prodColor") setProdColor(e.target.value);
+    if (e.target.name === "prodCategory") setProdCategory(e.target.value);
+    if (e.target.name === "productFeatured") setProductFeatured(e.target.value);
+  };
+
+  const handleFileChange = (e, setImage) => {
+    setImage(e.target.files[0]);
+  };
+  const updateProd = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("prodTitle", prodTitle);
+    formData.append("prodDesc", prodDesc);
+    formData.append("prodPrice", prodPrice);
+    formData.append("prodQty", prodQty);
+    formData.append("prodColor", prodColor);
+    formData.append("prodSize", prodSize);
+    formData.append("prodCategory", prodCategory);
+    formData.append("productFeatured", productFeatured);
+    formData.append("prodImg1", prodImg1);
+    formData.append("prodImg2", prodImg2);
+
+    try {
+      let response = await fetch(
+        `http://localhost:5000/api/prod/v1/updateproduct/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            "auth-token":
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6IjY1ODMzZWRiZjU4MzE3ZGMzYzg5MTQyMCIsImlhdCI6MTcwMzEwMDE1OH0.xqQ7ykJAGEwW_-DOuTps0GCpK4nU4Hra40d-g4TGGR8", // this token comes from you cookies or localstorage
+          },
+          body: formData,
+        }
+      );
+      const data = await response.json();
+      if (data.success) {
+        formData(" ");
+        alert(`${data.message}`);
+      } else {
+        alert(`${data.message}`);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      console.log(error);
+      alert("Something went wrong!");
+    }
+  };
+
+  // For Delete Prod
+  const deleteProd = async (id) => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/prod/v1/deleteproduct/${id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "auth-token":
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6IjY1ODMzZWRiZjU4MzE3ZGMzYzg5MTQyMCIsImlhdCI6MTcwMzEwMDE1OH0.xqQ7ykJAGEwW_-DOuTps0GCpK4nU4Hra40d-g4TGGR8", // this token comes from you cookies or localstorage
+          },
+        }
+      );
+      let res = await response.json();
+      if (res.success) {
+        let updateProdList  = product.filter(x =>x._id!== id)
+        setProduct(updateProdList)
+        alert(res.message);
+      } else {
+        alert(res.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // if loading equal to true then we show loading ...
+  if (loading) {
+    return <h1 className="text-xl m-5 p-5 font-bold">Loading....</h1>;
   }
-  useEffect(()=>{
-    getProduct()
-  },[])
+
   return (
     <>
       <div className="add_search">
@@ -261,10 +342,12 @@ const [loading  , setLoading]= useState(true)
                         <FontAwesomeIcon icon={faEllipsisVertical} />
                       </DropdownToggle>
                       <DropdownMenu>
-                        <DropdownItem onClick={() => deleteProduct(index)}>
+                        <DropdownItem onClick={() => deleteProd(item._id)}>
                           Delete
                         </DropdownItem>
-                        <DropdownItem onClick={() => openEditModal(index)}>
+                        <DropdownItem
+                          onClick={() => openEditModal(index, item._id, item)}
+                        >
                           Edit
                         </DropdownItem>
                         <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown}>
@@ -308,88 +391,184 @@ const [loading  , setLoading]= useState(true)
       >
         <ModalHeader toggle={toggleEditModal}>Edit Product</ModalHeader>
         <ModalBody>
-          <Table className="Table-Edit">
-            <thead>
-              <tr>
-                <th>Image</th>
-                <th>Title</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Color</th>
-                <th>Category</th>
-              </tr>
-            </thead>
-            <tbody>
-              <th className="img-file">
-                <img
-                  htmlFor="fileInput"
-                  className="custom-file-upload"
-                  src={editImage}
-                  alt="Edited Product"
-                  style={{ maxWidth: "80px", cursor: "pointer" }}
-                  onClick={() => document.getElementById("fileInput").click()}
-                />
-                <input
-                  id="fileInput"
-                  type="file"
-                  style={{ display: "none" }}
-                  onChange={handleImageChange}
-                />
-              </th>
-              <td>
-                <input
-                  type="text"
-                  value={editName}
-                  onChange={(e) => setEditName(e.target.value)}
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={editPrice}
-                  onChange={(e) => setEditPrice(e.target.value)}
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={editName}
-                  onChange={(e) => setEditName(e.target.value)}
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={editPrice}
-                  onChange={(e) => setEditPrice(e.target.value)}
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={editName}
-                  onChange={(e) => setEditName(e.target.value)}
-                />
-              </td>
-            </tbody>
-          </Table>
+          <form className="">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="mb-3 flex flex-col">
+                  <label for="product name" className="form-label">
+                    Product Title
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control border-2 border-slate-200 rounded mt-2"
+                    id="productname"
+                    value={prodTitle}
+                    name="prodTitle"
+                    onChange={handChange}
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="mb-3 flex flex-col">
+                  <label for="productcategory" className="form-label">
+                    Product Description
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control border-2 border-slate-200 rounded mt-2"
+                    id="productcategory"
+                    value={prodDesc}
+                    name="prodDesc"
+                    onChange={handChange}
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="mb-3 flex flex-col">
+                  <label for="productcompany" className="form-label">
+                    Product Price
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control border-2 border-slate-200 rounded mt-2"
+                    id="productcompany"
+                    value={prodPrice}
+                    name="prodPrice"
+                    onChange={handChange}
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="mb-3 flex flex-col">
+                  <label for="productqunatity" className="form-label">
+                    Product Quantity
+                  </label>
+                  <input
+                    type="Number"
+                    className="form-control border-2 border-slate-200 rounded mt-2"
+                    id="productquantity"
+                    value={prodQty}
+                    name="prodQty"
+                    onChange={handChange}
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="mb-3 flex flex-col">
+                  <label for="productdiscount" className="form-label">
+                    Product Color
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control border-2 border-slate-200 rounded mt-2"
+                    id="productdiscount"
+                    value={prodColor}
+                    name="prodColor"
+                    onChange={handChange}
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="mb-3 flex flex-col">
+                  <label for="productExtradiscount" className="form-label">
+                    Product Size
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control border-2 border-slate-200 rounded mt-2"
+                    id="productextradiscount"
+                    value={prodSize}
+                    name="prodSize"
+                    onChange={handChange}
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="mb-3 flex flex-col">
+                  <label for="productExtradiscount" className="form-label">
+                    Product Category
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control border-2 border-slate-200 rounded mt-2"
+                    id="productextradiscount"
+                    value={prodCategory}
+                    name="prodCategory"
+                    onChange={handChange}
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="mb-3 flex flex-col">
+                  <label for="productExtradiscount" className="form-label">
+                    Product Category
+                  </label>
+                  <select
+                    id="cars"
+                    className="form-control border-2 border-slate-200 rounded mt-2"
+                    value={productFeatured}
+                    name="productFeatured"
+                    onChange={handChange}
+                  >
+                    <option value="null">~Select feature~ </option>
+                    <option value="best-deals">Best Deals</option>
+                    <option value="featured">Featured</option>
+                    <option value="on-sale">On Sale</option>
+                    <option value="top-rated">Top Rated</option>
+                    <option value="entertainment">Entertainment</option>
+                    <option value="electronic">Electronic</option>
+                    <option value="new-arrivals">New Arrivals</option>
+                    <option value="tv-led">Tv and LED</option>
+                    <option value="laptop-computer">Laptop & Computer</option>
+                    <option value="recommended">Recommended</option>
+                  </select>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="mb-3 flex flex-col">
+                  <label for="productExtradiscount" className="form-label">
+                    Product Image1
+                  </label>
+                  <input
+                    type="file"
+                    className="form-control border-2 border-slate-200 rounded mt-2"
+                    id="prodImg1"
+                    name="prodImg1"
+                    onChange={(e) => handleFileChange(e, setProdImg1)}
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="mb-3 flex flex-col">
+                  <label for="productExtradiscount" className="form-label">
+                    Product Image2
+                  </label>
+                  <input
+                    type="file"
+                    className="form-control border-2 border-slate-200 rounded mt-2"
+                    id="prodImg2"
+                    name="prodImg2"
+                    onChange={(e) => handleFileChange(e, setProdImg2)}
+                  />
+                </div>
+              </div>
+            </div>
+            <button
+              style={{ background: "red" }}
+              type="submit"
+              className="btn addcolor p-2 text-white rounded mt-2"
+              onClick={updateProd}
+            >
+              Add Product
+            </button>
+          </form>
         </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={saveEditedProduct}>
-            Save
-          </Button>
-          <Button color="secondary" onClick={toggleEditModal}>
-            Cancel
-          </Button>
-        </ModalFooter>
       </Modal>
       <Modal isOpen={addModal} toggle={toggleAddModal} className="modals-input">
         <ModalHeader toggle={toggleAddModal}>Add New Product</ModalHeader>
 
         <ModalBody>
-          <div className="container">
-      
-          </div>
+          <div className="container"></div>
         </ModalBody>
         <ModalFooter>
           <Button color="primary" onClick={handleAddProduct}>
